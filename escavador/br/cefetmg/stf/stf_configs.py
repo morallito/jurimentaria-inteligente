808a77197a59e97c56154e94c6be4a869d3a1b4a
morallito <iagohenrique22@hotmail.com>
2023-09-04 11:10:51 +0000

    add get number of results

--- escavador/br/cefetmg/stf/stf_configs.py
+++ escavador/br/cefetmg/stf/stf_configs.py
@@ -1,10 +1,13 @@
 SEARCH_URL = 'https://jurisprudencia.stf.jus.br/pages/search?base=acordaos&pesquisa_inteiro_teor=false&sinonimo=true' \
-             '&plural=true&radicais=false&buscaExata=true&page=<PAGE_NUMBER>&pageSize=10&queryString=<QUERY_STRING' \
-             '>&sort=_score&sortBy=desc'
+             '&plural=true&radicais=false&buscaExata=true&page=<PAGE_NUMBER>&pageSize=1&queryString=<QUERY_STRING>' \
+             '&sort=_score&sortBy=desc'
 SEARCH_FIELD = '<QUERY_STRING>'
 PAGE_FIELD = '<PAGE_NUMBER>'
 
+XPATH_NUMBER_OF_RESULTS="//span[@class='pages-resume ml-0']"
+
 # Delay in seconds to wait the main page to load
 HOMEPAGE_IMPLICIT_WAITING_TIME = 1
-SEARCH_PAGE_IMPLICIT_WAITING_TIME = 3
+SEARCH_PAGE_IMPLICIT_WAITING_TIME = 5
+
 
--- escavador/br/cefetmg/stf/stf_scrapper.py
+++ escavador/br/cefetmg/stf/stf_scrapper.py
@@ -2,13 +2,13 @@
 import logging
 
 from .stf_configs import SEARCH_URL, SEARCH_FIELD, HOMEPAGE_IMPLICIT_WAITING_TIME, PAGE_FIELD, \
-SEARCH_PAGE_IMPLICIT_WAITING_TIME
+SEARCH_PAGE_IMPLICIT_WAITING_TIME, XPATH_NUMBER_OF_RESULTS
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
 
 class stf_scrapper():
 
@@ -21,17 +21,48 @@ class stf_scrapper():
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-extensions'); # disabling extensions
         chrome_options.add_argument("--remote-debugging-port=9222"); # enable debug port 
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0"
+        chrome_options.add_argument(f'--user-agent={user_agent}')
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
         self.driver.implicitly_wait(HOMEPAGE_IMPLICIT_WAITING_TIME)
+
+    def clean_search_query(self, query):
+        """
+        Clean the query, transform it into a valid url parameter
+        """
+        return query.replace(' ', '%20')
 
 
     def perform_simple_stf_search(self, query):
         """
         Performs a simple search on the STF website, and returns the results
         """
+        query = self.clean_search_query(query)
         URL = SEARCH_URL.replace(SEARCH_FIELD, query)
         print(f'Performing search at {URL}...')
 
-        self.driver.get(URL)
+        result_number = self.get_search_result_info(query)
+        print(f'Found {result_number} results.')
+
+    def formatUrl(self, query, page):
+        URL = SEARCH_URL.replace(SEARCH_FIELD, query)
+        URL = URL.replace(PAGE_FIELD, page)
+        return URL
+
+    def parse_string_number_of_results(self, string_number):
+        list_string = string_number.split(' ')
+        result = list_string[1]
+        result = result.replace('.', '')
+        return int(result)
+
+    def get_search_result_info(self, query):
+        """
+        Returns the number of results found by the search
+        """
+        search_url = self.formatUrl(query, '1') # First page
+        print(f'Using base url: {search_url}.')
+        self.driver.get(search_url)
         self.driver.implicitly_wait(SEARCH_PAGE_IMPLICIT_WAITING_TIME)
-        return self.driver.page_source
+        result_number = self.driver.find_element(By.XPATH, XPATH_NUMBER_OF_RESULTS)
+        print(self.parse_string_number_of_results(result_number.text))
+        return result_number.text
